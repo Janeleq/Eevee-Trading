9dfb696a79ec4c543528a0aa8f328d6e1823956b
Janeleq <janelim2001@gmail.com>
2023-03-22 18:24:33 +0800

    Update pricing.py

--- Backend/pricing.py
+++ Backend/pricing.py
@@ -1,1 +1,7 @@
 # Pricing Microservice
+
+import requests
+
+# invoke Crytocompare API to get different pricing according to specific CC
+response = requests.get("http://randomfox.ca/floof")
+print(response.status_code, response.json())
