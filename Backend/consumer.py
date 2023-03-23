2fbfe78ef96bbf4ddda89b9609c6b35a2b4eb223
Janeleq <janelim2001@gmail.com>
2023-03-24 01:17:31 +0800

    Delete consumer.py

--- Backend/consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-
-
-TOPIC_NAME = 'orders'
-
-consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=['localhost:9092'], api_version=(0,11,5))
-
-for message in consumer:
-    print(message)
