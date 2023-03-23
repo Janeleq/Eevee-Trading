378d5e664a2e56f7eb4ad545735f707ad15cba86
Janeleq <janelim2001@gmail.com>
2023-03-24 01:17:42 +0800

    Update wallet.py

--- Backend/wallet.py
+++ Backend/wallet.py
@@ -1,1 +1,32 @@
 # Complex Wallet Microservice
+
+# from app import p
+
+
+
+def checkWallet():
+    # if wallet enough $$, make the transaction by calling UpdateWallet function and notify user of sucess
+
+    
+    if __:
+        return jsonify (
+            {
+                "code": 200,
+                "data":
+                {
+                    "status": "suce"
+                }
+                "message": "Sucessful Transaction!"
+            }
+        )
+
+    # else, reject the transaction and notify user
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Unsucessful Transaction!"
+        }
+    )
+    
+
+def UpdateWallet():
