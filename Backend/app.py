dc85c9e87dc71100064b452dd82b2c08a776bc6b
Jane lim <janelim2001@gmail.com>
2023-04-01 15:17:36 +0800

    Update File

--- Backend/app.py
+++ Backend/app.py
@@ -9,10 +9,8 @@ import stripe
 # from invokes import invoke_http
 
 app = Flask(__name__, template_folder='../Frontend/templates', static_folder='../Frontend/static')
-app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51Mqv3mL81p6Fg6ebcfrJYprowuiyEYky8iILawOUGwdd7WEjxkQk6hJRfSXm02XdbgzBU0qGmhJxoA737LI0mDcm004m87jVGX'
-app.config['STRIPE_SECRET_KEY'] = 'sk_test_51Mqv3mL81p6Fg6ebxNqIERpNmaW1FIyE0Ps6EH6A3UHKI9pMVIlUR6ExCmOwlrrBXArZPTLu0GnF8wOppX16g2qq00hB17R6OX'
 
-stripe.api_key = app.config['STRIPE_SECRET_KEY']
+
 # function to fetch hostname and ip to check different instances currently running 
 def fetchDetails():
     hostname = socket.gethostname()
@@ -213,39 +211,15 @@ def solana():
 #     # result = invoke_http("http:localhost:5000/transaction", method='POST')
 #     # print(result)
 
-@app.route('/thanks')
-def thanks():
-    return render_template('thanks.html')
-
-@app.route("/topup", methods=['POST'])
-def topUpWallet():
-    
-    status = '\n --- Bringing you to top-up page ---'
-    try: 
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                    'price': 'price_1MqvPOL81p6Fg6ebILbmwrSq',
-                    'quantity': 1,
-                }],
-            mode='payment',
-            success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url=url_for('profile', _external=True),
-        )
-    except Exception as e:
-        return str(e)
-    # render_template( 
-    #         'coins/profilepage.html',
-    #         checkout_session_id=session['id'],
-    #         checkout_public_key=['STRIPE_PUBLIC_KEY']
-    #         )
-
-    return redirect(session.url, code=303)
-    # return redirect("https://buy.stripe.com/test_00g5nJ2nW3GC1zO288")
-    # result = invoke_http("http:localhost:5000/transaction", method='POST')
-    # print(result)
-
-
+topUpURL = 'http://localhost:5005/processtopup'
+@app.route('/topup')
+def topup():
+    print("\n--- Invoking Topup Microservice ---")
+    response = requests.get(topUpURL)
+    if response:
+        return 'Hi'
+    else:
+        return 'Bye'
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
